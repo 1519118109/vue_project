c9c8d4c1e9cf84be4c5781fde20e45c58bed7ec5
xiefeng <1519118109@qq.com>
2020-09-08 08:43:51 +0800

    Initial commit

---
+++ src/components/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        这是index界面===访问成功
+        <br>
+        姓名：{{name}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data:function () {
+            return{
+                name:'小明',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        color: green;
+    }
+</style>
